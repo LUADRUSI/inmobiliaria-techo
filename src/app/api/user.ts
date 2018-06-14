f7f0de837cab5b8754c7526db148907b79566418
LUADRUSI <luadrusi@yahoo.com>
2018-06-14 17:09:16 -0500

    }Se incorporan librerias y componentes

---
+++ src/app/api/user.ts
@@ -0,0 +1,5 @@
+export class user {
+   id: number;
+   firstName: string;
+   lastName: string ;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GetUsersComponent } from "./get-users/get-users.component";
+import { PostUsersComponent } from "./post-users/post-user.component";
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { HttpClientModule } from "@angular/common/http";
+import { MatButtonModule, MatCheckboxModule, MatIconModule, MatInputModule, MatMenuModule, MatCardModule } from "@angular/material";
+import { MatListModule } from "@angular/material/list";
+import { MatTabsModule} from "@angular/material/tabs";
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetUsersComponent,
+    PostUsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HttpModule,
+    MatCheckboxModule,
+    MatIconModule,
+    FormsModule,
+    MatMenuModule,
+    MatCardModule,
+    MatTabsModule,
+    MatInputModule,
+    MatListModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
